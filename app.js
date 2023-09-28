bf75c47a9e9b977cb7ca370160e6498a8f557fbe
AnasKhan20 <143949011+AnasKhan20@users.noreply.github.com>
2023-09-28 03:34:15 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,74 @@
+// CHAPTER 21(CHANGING CASE)
+
+// question 1
+
+// var userInput = prompt("Enter a character");
+// var text = userInput.toLowerCase()
+
+// var arr = ["KARACHI","LAHORE"]
+
+// console.log("match")
+
+// Questionn 2
+
+// var x = "HELLO";
+// var text = x.toLowerCase()
+
+// console.log(text)
+
+// Question 3
+
+// var y = "hello";
+// var text = y.toUpperCase()
+
+// console.log(text)
+
+// Question 4
+
+// var userInput = prompt("Enter a string");
+// var text = userInput.toLowerCase()
+
+// console.log(text)
+
+// Question 5
+
+// var arr = ["KARACHI","LAHORE"]
+// var text = arr.toLowerCase()
+
+// console.log(arr[0])
+
+
+// Question 6
+
+// var userInput = prompt("Enter a string")
+// var text = userInput.toUpperCase()
+
+// alert(text)
+
+
+
+// CHAPTER 22-25
+
+
+// Question 1
+
+// var sameWords = "captain"
+// var cut = sameWords.slice(1,3)
+// console.log(cut)
+
+// Question 3
+
+// var animal = "elephant"
+// var seg = animal.slice(2,6)
+// console.log(seg)
+
+// Question 6
+
+// var text = "to be or not to be"
+// var indx = text.indexOf("be")
+// console.log(indx)
+
+// Question 8
+
+
+
